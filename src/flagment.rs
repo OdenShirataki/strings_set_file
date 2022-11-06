f1be36995ea6bf03af0fe953feb7515ec7db53c2
OdenShirataki <git config user.email>
2022-11-06 15:24:20 +0900

    bug fix

--- src/flagment.rs
+++ src/flagment.rs
@@ -11,46 +11,34 @@ pub(super) struct FragmentGetResult{
 pub(super) struct Fragment{
     filemmap:FileMmap
     ,list: ManuallyDrop<Box<DataAddress>>
-    ,record_count:u64
+    ,record_count:ManuallyDrop<Box<u64>>
 }
 const DATAADDRESS_SIZE:usize=std::mem::size_of::<DataAddress>();
+const COUNTER_SIZE:usize=std::mem::size_of::<u64>();
+const INIT_SIZE:usize=COUNTER_SIZE+DATAADDRESS_SIZE;
 impl Fragment{
     pub fn new(path:&str)->Result<Self,std::io::Error>{
-        let init_size=DATAADDRESS_SIZE as u64;
-        let filemmap=FileMmap::new(path,init_size)?;
-        let list=filemmap.as_ptr() as *mut DataAddress;
-        let len=filemmap.len();
-        let mut record_count=if len==init_size{
-            0
-        }else{
-            (len-init_size)/DATAADDRESS_SIZE as u64 - 1
-        };
-        if record_count>0{
-            for i in -(record_count as i64)..0{
-                let index=(-i) as u64;
-                if unsafe{*list.offset(index as isize)}.offset==0{
-                    record_count=index-1;
-                }
-            }
-        }
+        let filemmap=FileMmap::new(path,INIT_SIZE as u64)?;
+        let list=filemmap.offset(COUNTER_SIZE as isize) as *mut DataAddress;
+        let counter=filemmap.as_ptr() as *mut u64;
         Ok(Fragment{
             filemmap
             ,list:ManuallyDrop::new(unsafe{Box::from_raw(list)})
-            ,record_count
+            ,record_count:ManuallyDrop::new(unsafe{Box::from_raw(counter)})
         })
     }
     pub fn insert(&mut self,ystr:&DataAddress)->Result<u64,std::io::Error>{
-        self.record_count+=1;
+        **self.record_count+=1;
         let size=
-            (DATAADDRESS_SIZE as u64)*(1+self.record_count)
+            INIT_SIZE+DATAADDRESS_SIZE * **self.record_count as usize
         ;
-        if self.filemmap.len()<size{
+        if self.filemmap.len()<size as u64{
             self.filemmap.set_len(size as u64)?;
         }
         unsafe{
-            *(&mut**self.list as *mut DataAddress).offset(self.record_count as isize)=*ystr;
+            *(&mut**self.list as *mut DataAddress).offset(**self.record_count as isize)=*ystr;
         }
-        Ok(self.record_count)
+        Ok(**self.record_count)
     }
     pub fn release(&mut self,row:u64,len:u64){
         let mut s=unsafe{
@@ -59,15 +47,15 @@ impl Fragment{
         s.offset+=len as i64;
         s.len-=len;
 
-        if s.len==0 && row==self.record_count{
-            self.record_count-=1;
+        if s.len==0 && row==**self.record_count{
+            **self.record_count-=1;
         }
     }
     pub fn search_blank(&self,len:u64)->Option<FragmentGetResult>{
-        if self.record_count==0{
+        if **self.record_count==0{
             None
         }else{
-            for i in -(self.record_count as i64)..0{
+            for i in -(**self.record_count as i64)..0{
                 let index=(-i) as u64;
                 let s=unsafe{
                     &*(&**self.list as *const DataAddress).offset(index as isize)
